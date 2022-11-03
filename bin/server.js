d704dc14c7a77a921a6b75e2e3645507ab713b5d
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-11-03 19:00:00 -0300

    feat: Estrutura de pastas e server.js

---
+++ bin/server.js
@@ -0,0 +1,15 @@
+const app = require("../src/app");
+const port = normalizaPort(process.env.PORT || "3000");
+function normalizaPort(value) {
+  const port = parseInt(value, 10);
+  if (isNaN(port)) {
+    return value;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
+app.listen(port, function () {
+  console.log("app listening on port ${port}");
+});
